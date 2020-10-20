d057c3f89dc843572081160148f2a42d466703f8
Abner Rodrigues <abnerluis1001@gmail.com>
2020-10-20 12:51:21 -0300

    feat: added hover styles to links

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -5,10 +5,14 @@ export default createGlobalStyle`
     margin: 0;
     padding: 0;
     box-sizing: border-box;
+  
   }
   body {
     background: ${props => props.theme.colors.background};
     color: ${props => props.theme.colors.text};
     font: 400 16px Roboto, sans-serif;
+    text-rendering: optimizeLegibility;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
   }
 `
--- src/styles/pages/Song.ts
+++ src/styles/pages/Song.ts
@@ -24,12 +24,24 @@ export const Button = styled.button`
   background-color: ${props => props.theme.colors.primary};
   font: 700 16px Roboto, sans-serif;
   cursor: pointer;
+  transition: 0.4s;
 
   & ::before {
     font-family: 'Font Awesome 5 Free';
     font-weight: 900;
     content: '\f060';
     padding-right: 1ch;
+    transition: 0.25s;
+  }
+
+  & :hover {
+    padding: 1.25ch 1.55ch;
+    transform: translateY(-1px);
+    box-shadow: 0 7px 14px rgba(50, 50, 93, 0.1), 0 3px 6px rgba(0, 0, 0, 0.08);
+
+    & ::before {
+      padding-right: 1.5ch;
+    }
   }
 `
 
@@ -39,6 +51,8 @@ export const ListItem = styled.li`
   list-style-type: none;
   margin-bottom: 2ch;
   cursor: pointer;
+  transition: 0.2s;
+
   h2 {
     color: ${props => props.theme.colors.primary};
   }
@@ -48,5 +62,15 @@ export const ListItem = styled.li`
     height: 10ch;
     margin-right: 3ch;
     border-radius: 10%;
+    transition: 0.4s;
+  }
+
+  & :hover {
+    padding: 1.25ch;
+    box-shadow: 0 7px 14px rgba(50, 50, 93, 0.1), 0 3px 6px rgba(0, 0, 0, 0.08);
+
+    img {
+      border-radius: 15%;
+    }
   }
 `

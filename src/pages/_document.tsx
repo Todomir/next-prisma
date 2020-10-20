6943b6ef3d37f582ada96280f3e8298fc3980bfa
Abner Rodrigues <abnerluis1001@gmail.com>
2020-10-20 09:06:28 -0300

    feat: added font-awesome css to head

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -45,6 +45,12 @@ export default class MyDocument extends Document {
             href="https://fonts.googleapis.com/css?family=Roboto:400,500,700"
             rel="stylesheet"
           />
+          <link
+            rel="stylesheet"
+            href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css"
+            integrity="sha512-+4zCK9k+qNFUR5X+cKL9EIR+ZOhtIloNl9GIKS57V1MyNsYpYcUrUeQc9vNfzsWfV28IaLL3i96P9sdNyeRssA=="
+            crossOrigin="anonymous"
+          />
         </Head>
         <body>
           <Main />
